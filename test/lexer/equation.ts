8a2eebbed460467861ac47c57ca851eb5a9fecd3
Ryan Leonard <rhinoisnot@gmail.com>
2019-03-01 10:37:56 -0500

    Drafted lexer.

---
+++ test/lexer/equation.ts
@@ -0,0 +1,62 @@
+import test from "ava";
+import * as lexer from "../../lexer";
+
+test("can contain numbers", t => {
+    lexer.reset("(5)");
+    t.is(lexer.next().type, "startEquation");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "endEquation");
+    t.is(lexer.next(), undefined);
+});
+
+test("can contain strings", t => {
+    lexer.reset(`("test")`);
+    t.is(lexer.next().type, "startEquation");
+    t.is(lexer.next().type, "startString");
+    t.is(lexer.next().type, "text");
+    t.is(lexer.next().type, "stringEnd");
+    t.is(lexer.next().type, "endEquation");
+    t.is(lexer.next(), undefined);
+});
+
+test("can include equations", t => {
+    lexer.reset(`(5+10)`);
+    t.is(lexer.next().type, "startEquation");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "plus");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "endEquation");
+    t.is(lexer.next(), undefined);
+});
+
+test("can include spaces", t => {
+    lexer.reset(`(5 + 10)`);
+    t.is(lexer.next().type, "startEquation");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "ws");
+    t.is(lexer.next().type, "plus");
+    t.is(lexer.next().type, "ws");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "endEquation");
+    t.is(lexer.next(), undefined);
+});
+
+test("can be nested", t => {
+    lexer.reset(`(5 + (10 * 2))`);
+    t.is(lexer.next().type, "startEquation");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "ws");
+    t.is(lexer.next().type, "plus");
+    t.is(lexer.next().type, "ws");
+
+    t.is(lexer.next().type, "startEquation");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "ws");
+    t.is(lexer.next().type, "multiply");
+    t.is(lexer.next().type, "ws");
+    t.is(lexer.next().type, "uint");
+    t.is(lexer.next().type, "endEquation");
+
+    t.is(lexer.next().type, "endEquation");
+    t.is(lexer.next(), undefined);
+});
